40e5355af1f05846c6076dbbe2486590d8f6e1b1
Lakshan-16 <4al21ec042@gmail.com>
2025-03-10 11:53:20 +0530

    new commit

--- script.js
+++ script.js
@@ -161,7 +161,215 @@
 // for (let i = n; --n; i += n);
 
 // console.log("Sum of first", n, "natural numbers is:", i);
-let n=10;
-let i=n;
-for(i;--n;i+=n);
-console.log("sum:",i);
+// let n=10;
+// let i=n;
+// for(i;--n;i+=n);
+// console.log("sum:",i);
+
+
+// -----------------------------------------------------
+// for-of loop , iterate over arrays and strings , it helps 
+// let str = "Lakshan";
+// for(let i of str) {
+//     console.log("i=",i);
+
+// }
+
+// let size =0;
+// for(let i of str){
+//     console.log("i=",i);
+//     size++;
+// }
+// console.log(size);
+
+
+// ----------------------------------------------------------
+// for in loop is used for iteration on Objects 
+// for(let key in objVar){
+
+// }
+
+
+// const student ={
+
+//     name : "Lakshan", 
+//     age :22,
+//     course :"ECE"
+
+// };
+
+// for(let key in student){
+//     console.log("keys =",key,"value=",student[key]);  // here student["key "] or student.key wont work , becuase the 'key' is not actual the key address . here key is a index variable 
+// }
+
+// even numbers form 0 to 100
+
+// for(let i=0;i<=100 ;i+=2){
+//     console.log(i);
+// }
+
+// using while loop 
+// let i=0;
+// while (i<=100){
+//     console.log(i);
+//     i+=2;
+// }
+
+// do -while loop 
+// let i=0;
+
+// do{
+//      console.log(i);
+//     i+=2;
+// } while(i<=100);
+
+// guess the number 
+
+// let gameNum =25;
+
+// let userNo = prompt("guess a number");
+// while(gameNum != userNo){
+//     console.log("try again");
+//     userNo = prompt("guess a number");
+// }
+// console.log("you guessed it right");
+
+// String in js 
+// let str="Lakshan"; // '' can also be used
+
+//  in-built property or internal variable 
+// 1. length //str.length
+
+// Templet leterals 
+// let specialString = `this is a templet leteral`;
+// console.log(specialString);
+
+
+// const student ={
+//     name : "Lakshan", 
+//     age :22,
+//     course :"ECE"
+    
+// };
+
+// console.log("name:",student.name,"age:",student.age,"course:",student.course);
+// console.log(" OR ");
+// console.log(`Name: ${student.name} age: ${student.age} course ${student.course}`);
+
+// ESCAPE CHARECTER 
+// \n new line
+// \t tab space
+
+// console.log("hello \n world");
+
+// the escape charecter \n or \t are considerd as single charecter of lenth 1 ;
+
+//String methods 
+
+// str.toUpperCase() , str.toLowerCase() , str.trim() // removes whitespaces 
+
+// let s = "Lakshan" ;
+// console.log(s);
+// console.log(s.toUpperCase());
+
+// strings are immutable in JS similar to JAVA
+
+// trims strating and ending white spaces in a string 
+
+// let s= "    hello     world       ";
+// console.log(s.trim());
+
+
+//str.slice(start ,end); // returns part of a string // the end index is non inclusive 
+// str.slice(strat) ; this also works 
+
+// let str ="Lakshan";
+// console.log(str.slice(3,6)); // prints sha
+
+
+// // concatinate  using method 
+// let str1="hello";
+// let str2="world";
+// let str3=str1.concat(str2);
+// console.log(str3);
+// // concatinate using +
+// let str4 = str1 +str2;
+// console.log(str4);
+
+// str.replace(searchVal, newVal);
+
+// let str = "hello";
+// console.log(str.replace("h","y"));
+// console.log(str.replace("lo","p"));
+
+// str.replaceAll (serachValue,newVal);
+// let s ="Lakshan";
+// console.log(s.charAt(0));   
+
+// // problem Prompt the user to enter their full name. Generate a username for them based on the input 
+// // Start username with @, followed by their full name and ending with the fullname length
+
+// let user = prompt("enter your name ");
+// let username = `@${user}${user.length}`;
+// console.log(username);
+
+
+// let names =["alia","thania","saloni","neha"];
+// let mix =[400,"delisha ", "ashlie",832];
+// for(let i =0;i<names.length;i++){
+//     console.log(names[i]);
+// }
+// for(let ele of names){
+//     console.log(ele);
+// }
+
+// for(let ele in names){
+//     console.log(names[ele]);
+// }
+
+// let marks =[89 ,99 ,80,100];
+
+// for(let i =1;i<marks.length;i++){
+//     marks[0]+=marks[i];
+// }
+// let avg = marks[0]/marks.length;
+// console.log(avg);
+
+// Array methods 
+// Arrays are mutable 
+// some methods return new array 
+// some methods modify the same 
+// 1.push 
+// 1.pop 
+
+// exaple 
+let veg =["carrot","tomato","onion","potato","dragon fruit"];
+console.log(veg);
+// if we want to push element to the end 
+veg.push("papaya");
+console.log(veg);
+// if we want to pop element form the end 
+veg .pop(); // this will pop papaya 
+console.log(veg);
+
+// push multiple items 
+veg.push("apple", "orange","banana");
+console.log(veg);
+
+// toString method converts array to string 
+console.log(veg.toString());
+// concat() joins multiple arrays and returns the combined arry and does no changes in the actual array 
+
+let marvel= ["iron man", "spider man" ," thor", " captian america" ];
+let dc =["super man","bat man","shazam ","Aqua man"];
+console.log(marvel);
+console.log(dc);
+console.log(marvel.concat(dc));
+
+// unshift() similar to push but addes element in starting 
+marvel.unshift("Ant man")
+console.log(marvel);
+
+// shift() this is similar to pop but deletes element from 
+console.log(" deleted", dc.shift());
+console.log(" new dc arrya is ",dc);
